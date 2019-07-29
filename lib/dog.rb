f85ef555031f32863e5042fcfe63977f48e39d04
Amy Westcott <amy.westcott@yahoo.com>
2019-07-29 17:09:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,4 +41,12 @@ class Dog
       self
     end 
    end 
+   
+   def update
+      sql_update = <<-SQL
+         UPDATE dogs SET name = ?, breed = ? WHERE id = ?;
+      SQL
+
+      DB[:conn].execute(sql_update, self.name, self.breed, self.id)
+   end
 end 
